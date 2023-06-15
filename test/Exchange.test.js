be56771ca7b8edc705057f09bbdf793009b7d6e9
KeFuCode <oker@okerdeMacBook-Pro.local>
2023-06-15 15:08:33 +0800

    simple uniswap v1: addLiquidity & get swap amount & eth/token swap

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -46,4 +46,70 @@ describe("Exchange", () => {
       expect(await exchange.getReserve()).to.equal(ethers.parseEther("200"));
     });
   });
+
+  describe("getPrice", async () => {
+    it("returns correct prices", async () => {
+      const { provider, token, exchange } = await loadFixture(
+        deployTokenAndExchangeFixture
+      );
+      const exchangeAddress = await exchange.getAddress();
+
+      await token.approve(exchangeAddress, ethers.parseEther("2000"));
+      await exchange.addLiquidity(ethers.parseEther("2000"), {
+        value: ethers.parseEther("1000"),
+      });
+
+      const tokenReserve = await exchange.getReserve();
+      const ethReserve = await provider.getBalance(exchangeAddress);
+
+      expect(await exchange.getPrice(ethReserve, tokenReserve)).to.equal(500);
+      expect(await exchange.getPrice(tokenReserve, ethReserve)).to.equal(2000);
+    });
+  });
+
+  describe("getTokenAmount", async () => {
+    it("returns correct token amount", async () => {
+      const { token, exchange } = await loadFixture(
+        deployTokenAndExchangeFixture
+      );
+      const exchangeAddress = await exchange.getAddress();
+
+      await token.approve(exchangeAddress, ethers.parseEther("2000"));
+      await exchange.addLiquidity(ethers.parseEther("2000"), {
+        value: ethers.parseEther("1000"),
+      });
+
+      let tokensOut = await exchange.getTokenAmount(ethers.parseEther("1"));
+      expect(tokensOut).to.equal(ethers.parseEther("1.998001998001998001"));
+
+      tokensOut = await exchange.getTokenAmount(ethers.parseEther("100"));
+      expect(tokensOut).to.equal(ethers.parseEther("181.818181818181818181"));
+
+      tokensOut = await exchange.getTokenAmount(ethers.parseEther("1000"));
+      expect(tokensOut).to.equal(ethers.parseEther("1000"));
+    });
+  });
+
+  describe("getEthAmount", async () => {
+    it("returns correct eth amount", async () => {
+      const { token, exchange } = await loadFixture(
+        deployTokenAndExchangeFixture
+      );
+      const exchangeAddress = await exchange.getAddress();
+
+      await token.approve(exchangeAddress, ethers.parseEther("2000"));
+      await exchange.addLiquidity(ethers.parseEther("2000"), {
+        value: ethers.parseEther("1000"),
+      });
+
+      let ethOut = await exchange.getEthAmount(ethers.parseEther("2"));
+      expect(ethOut).to.equal(ethers.parseEther("0.999000999000999"));
+
+      ethOut = await exchange.getEthAmount(ethers.parseEther("100"));
+      expect(ethOut).to.equal(ethers.parseEther("47.619047619047619047"));
+
+      ethOut = await exchange.getEthAmount(ethers.parseEther("2000"));
+      expect(ethOut).to.equal(ethers.parseEther("500"));
+    });
+  });
 });
